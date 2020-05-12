d2fc5db37ed24ff4a6e22e17476f28a8a39bd0d2
Blake Eakin <blake.eakin@pcc.edu>
2020-05-12 15:34:50 -0700

    Add print option.

--- jwt-edit.py
+++ jwt-edit.py
@@ -18,10 +18,11 @@ opt_parser = argparse.ArgumentParser()
 
 opt_parser.add_argument("-t","--token", required=True, help="JWT token")
 opt_parser.add_argument("-d", "--data", required=False, help="Space-separated fields to add or edit in the payload. Formatted field:value", nargs='+')
-opt_parser.add_argument("-i", "--iat", required=False, help="Issued at time, can be now to use current time or a specified Unix time.")
-opt_parser.add_argument("-x", "--exp", required=False, help="Expiration time, can be an offset in minutes from the iat by prefixing with + or a valid Unix time.")
+opt_parser.add_argument("-i", "--iat", required=False, help="Issued at time, can be now to use current time, or a specified Unix time.")
+opt_parser.add_argument("-x", "--exp", required=False, help="Expiration time, can be an offset in minutes from the iat by prefixing with +, or a valid Unix time.")
 opt_parser.add_argument("-a", "--alg", required=False, help="Signature encryption algorithm.", choices=alg_choices)
 opt_parser.add_argument("-k", "--key", required=False, help="Encryption key.")
+opt_parser.add_argument("-p", "--print", required=False, help="Print token contents.", action="store_true")
 
 args = opt_parser.parse_args()
 
@@ -40,6 +41,9 @@ if args.token:
 		except binascii.Error:
 			jwt_token[1] = jwt_token[1] + '='
 
+if args.print:
+	print(header, payload)
+
 if args.data:
 	for arg in args.data:
 		split_arg = arg.split(':')
@@ -47,6 +51,8 @@ if args.data:
 
 if args.alg:
 	header["alg"] = args.alg
+elif not args.alg:
+	header["alg"] = 'none'
 
 if args.iat:
 	if args.iat == "now":
@@ -63,6 +69,7 @@ if args.exp:
 
 if args.key:
 	enc_key = args.key
+
 
 final_header = str(base64.b64encode(bytes(json.dumps(header), 'latin-1')))
 final_header = final_header[2:len(final_header)-1].strip('=')
